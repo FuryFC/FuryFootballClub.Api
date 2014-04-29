9dd9778cb5589b1c17772d4c469678b64a1551d3
mostekcm <carlosmostek@gmail.com>
2014-04-28 21:26:54 -0500

    Try putting the sqlserver connection string in the appSettings section of web.config

--- FuryFootballClub.Core/Domain/Context.cs
+++ FuryFootballClub.Core/Domain/Context.cs
@@ -6,17 +6,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using FuryFootballClub.Core.Migrations;
+using System.Data.Common;
 
 namespace FuryFootballClub.Core.Domain
 {
     class Context : DbContext
     {
+        /* 
+         * In order to have this work with appharbor, we need to have a funky configuration
+         */
+        private static DbConnection GetConnection()
+        {
+            var contextConfig = System.Configuration.ConfigurationManager.ConnectionStrings["context"];
+            DbProviderFactory dbProviderFactory = DbProviderFactories.GetFactory(contextConfig.ProviderName);
+            DbConnection dbConnection = dbProviderFactory.CreateConnection();
+            dbConnection.ConnectionString = System.Configuration.ConfigurationManager.AppSettings["SQLSERVER_CONNECTION_STRING"];
+            return dbConnection;
+        }
+
         public DbSet<MatchFixture> MatchFixtures { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<UserClaim> UserClaims { get; set; }
 
         public Context():
-            base("Name=SQLSERVER_CONNECTION_STRING")
+            base(GetConnection(),true)
         {
         }
 
